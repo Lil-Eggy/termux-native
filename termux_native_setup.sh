1c3777eb07c4d11a0253a85833936f0ed442ddb1
Lil-Eggy <hafiq3255@gmail.com>
2025-05-03 23:01:11 +0800

    Update termux_native_setup.sh

--- termux_native_setup.sh
+++ termux_native_setup.sh
@@ -8,3 +8,6 @@ pkg install tur-repo
 pkg install firefox
 wget https://github.com/termux/termux-x11/releases/download/nightly/app-universal-debug.apk
 termux-open app-universal-debug.apk
+wget https://raw.githubusercontent.com/Lil-Eggy/termux-native/refs/heads/main/start_native
+chmod +x start_native
+echo now you can start native desktop by typing "./start_native".
